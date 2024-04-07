99d9859fdcd308a913b52630450b6a5e12dc27c8
Kimyee Lim <151904784+kimyeelim@users.noreply.github.com>
2024-04-07 13:59:44 +0700

    Update logic.java
    register and login

--- logic.java
+++ logic.java
@@ -11,6 +11,52 @@ public class StudentLogic {
 	
 	private ArrayList<StudentModel> stlist = new ArrayList<StudentModel>();
  	private Scanner sc = new Scanner(System.in);
+
+	public void register()
+	{
+		System.out.println("Enter username: ");
+		String username = sc.next();
+
+		System.out.println("Enter password: ");
+		String password = sc.next();
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("D:\\user_data.txt", true))) {
+            
+            writer.println(username + "," + password);
+            System.out.println("Registration successful");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+		
+	}
+	public void login() {
+	    System.out.println("Enter username: ");
+	    String username = sc.next();
+
+	    System.out.println("Enter password: ");
+	    String password = sc.next();
+
+	    try (Scanner fileScanner = new Scanner(new FileInputStream("D:\\user_data.txt"))) {
+	        boolean found = false;
+	        while (fileScanner.hasNextLine()) {
+	            String line = fileScanner.nextLine();
+	            String[] data = line.split(",");
+	            if (data[0].equals(username) && data[1].equals(password)) {
+	                found = true;
+	                break;
+	            }
+	        }
+	        if (found) {
+	            System.out.println("Login successful");
+	        } else {
+	            System.out.println("Invalid username or password");
+	        }
+	    } catch (FileNotFoundException e) {
+	        System.out.println("Error: " + e.getMessage());
+	    }
+	  
+	}
+
  	
 	public void addStudent()
 	{
